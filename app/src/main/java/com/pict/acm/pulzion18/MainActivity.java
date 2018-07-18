5e452942495745df158e249c4ad3ae4d96fe1b7c
kshitijpatil09 <kshitijpatil98@gmail.com>
2018-07-18 10:40:57 +0530

    Basic Grid

--- app/src/main/java/com/pict/acm/pulzion18/MainActivity.java
+++ app/src/main/java/com/pict/acm/pulzion18/MainActivity.java
@@ -4,27 +4,43 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 
 import com.pict.acm.pulzion18.adapters.EventsAdapter;
 import com.pict.acm.pulzion18.model.EventEntry;
 
 public class MainActivity extends AppCompatActivity {
-
+    RecyclerView eventsRecycler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView eventsRecycler = findViewById(R.id.event_list);
+        eventsRecycler = findViewById(R.id.event_list);
         eventsRecycler.setHasFixedSize(true);
 
-        StaggeredGridLayoutManager grid = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
-        grid.setGapStrategy(StaggeredGridLayoutManager.GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS);
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
-        eventsRecycler.setLayoutManager(grid);
+        setupRecyclerView();
 
         EventsAdapter adapter = new EventsAdapter(this, EventEntry.initEventsList());
         eventsRecycler.setAdapter(adapter);
+    }
+
+    private void setupRecyclerView() {
+        GridLayoutManager gridLayoutManager = (GridLayoutManager) eventsRecycler.getLayoutManager();
+        gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                switch (position % 6) {
+                    case 5:
+                        return 3;
+                    case 3:
+                        return 2;
+                    default:
+                        return 1;
+                }
+            }
+        });
+        eventsRecycler.addItemDecoration(new GridMarginDecoration(
+                getResources().getDimensionPixelSize(R.dimen.grid_item_spacing)));
+        eventsRecycler.setHasFixedSize(true);
     }
 }
